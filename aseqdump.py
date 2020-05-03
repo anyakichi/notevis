8a859fd3caaafc2311bb4b15f0f27c90f0b7a8da
INAJIMA Daisuke <inajima@gmail.com>
2020-05-03 17:59:55 +0900

    Format with black

--- aseqdump.py
+++ aseqdump.py
@@ -10,10 +10,10 @@ import colorsys
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtGui, QtCore
 import alsaseq
-alsaseq.client( 'Recorder', 1, 0, True )
-alsaseq.connectfrom( 0, 20, 0 )
-alsaseq.start()
 
+alsaseq.client("Recorder", 1, 0, True)
+alsaseq.connectfrom(0, 20, 0)
+alsaseq.start()
 
 
 running = True
@@ -38,7 +38,7 @@ class Note(object):
         self.velocity = velocity
         self.duration = duration
         self.interval = interval
-        self.hand = 'u'
+        self.hand = "u"
 
 
 def note_to_str(note):
@@ -89,7 +89,11 @@ class AseqThread(threading.Thread):
             timestamp = event[4][0] + event[4][1] / 1000000000.0
 
             with data_lock:
-                if data and not notes_on and timestamp - (data[-1][0] + data[-1][3]) > 1.5:
+                if (
+                    data
+                    and not notes_on
+                    and timestamp - (data[-1][0] + data[-1][3]) > 1.5
+                ):
                     data2 = []
 
                     prev = None
@@ -107,20 +111,20 @@ class AseqThread(threading.Thread):
                             continue
                         if d.note[:-1] == prev.note[:-1]:
                             if d.note[-1] < prev.note[-1]:
-                                d.hand = 'l'
-                                prev.hand = 'r'
+                                d.hand = "l"
+                                prev.hand = "r"
                             else:
-                                d.hand = 'r'
-                                prev.hand = 'l'
+                                d.hand = "r"
+                                prev.hand = "l"
                             prev = None
                         else:
                             prev = d
 
                     prev = None
                     for d in data2:
-                        if d.hand == 'l':
+                        if d.hand == "l":
                             continue
-                        if d.hand == 'u':
+                        if d.hand == "u":
                             prev = None
                             continue
                         if not prev:
@@ -131,9 +135,9 @@ class AseqThread(threading.Thread):
 
                     prev = None
                     for d in data2:
-                        if d.hand == 'r':
+                        if d.hand == "r":
                             continue
-                        if d.hand == 'u':
+                        if d.hand == "u":
                             prev = None
                             continue
                         if not prev:
@@ -145,8 +149,12 @@ class AseqThread(threading.Thread):
                     velocities = [d.velocity for d in data2]
                     durations = [d.duration for d in data2 if d.duration != 0]
                     intervals = [d.interval for d in data2 if d.interval != 0]
-                    rintervals = [d for d in data2 if d.interval_one_hand != 0 and d.hand == 'r']
-                    lintervals = [d for d in data2 if d.interval_one_hand != 0 and d.hand == 'l']
+                    rintervals = [
+                        d for d in data2 if d.interval_one_hand != 0 and d.hand == "r"
+                    ]
+                    lintervals = [
+                        d for d in data2 if d.interval_one_hand != 0 and d.hand == "l"
+                    ]
 
                     if durations and intervals:
                         mu_vel = statistics.mean(velocities)
@@ -159,27 +167,68 @@ class AseqThread(threading.Thread):
                         for i, d in enumerate(data2):
                             if not rintervals and not lintervals:
                                 if abs(d.interval - mu_int) > pstd_int * 1.5:
-                                    print("{}: {}: interval {:.4f} ({:.2f}%)".format(i, d.note, d.interval - mu_dur, (d.interval - mu_int) * 100 / mu_int))
+                                    print(
+                                        "{}: {}: interval {:.4f} ({:.2f}%)".format(
+                                            i,
+                                            d.note,
+                                            d.interval - mu_dur,
+                                            (d.interval - mu_int) * 100 / mu_int,
+                                        )
+                                    )
                             if abs(d.velocity - mu_vel) > pstd_vel * 2:
-                                print("{}: {}: velocity {:.4f} ({:.2f}%)".format(i, d.note, d.velocity - mu_vel, (d.velocity - mu_vel) * 100 / mu_vel))
+                                print(
+                                    "{}: {}: velocity {:.4f} ({:.2f}%)".format(
+                                        i,
+                                        d.note,
+                                        d.velocity - mu_vel,
+                                        (d.velocity - mu_vel) * 100 / mu_vel,
+                                    )
+                                )
                             if abs(d.duration - mu_dur) > pstd_dur * 2:
-                                print("{}: {}: duration {:.4f} ({:.2f}%)".format(i, d.note, d.duration - mu_dur, (d.duration - mu_dur) * 100 / mu_dur))
+                                print(
+                                    "{}: {}: duration {:.4f} ({:.2f}%)".format(
+                                        i,
+                                        d.note,
+                                        d.duration - mu_dur,
+                                        (d.duration - mu_dur) * 100 / mu_dur,
+                                    )
+                                )
 
                         if rintervals:
                             d = [i.interval_one_hand for i in rintervals]
                             mu = statistics.mean(d)
                             pstd = statistics.pstdev(d, mu)
-                            print("interval (right): mean = {:.4f}, pstdev = {:.4f} ({:.2f}%)".format(mu, pstd, pstd * 100 / mu))
+                            print(
+                                "interval (right): mean = {:.4f}, pstdev = {:.4f} ({:.2f}%)".format(
+                                    mu, pstd, pstd * 100 / mu
+                                )
+                            )
 
                         if lintervals:
                             d = [i.interval_one_hand for i in lintervals]
                             mu = statistics.mean(d)
                             pstd = statistics.pstdev(d, mu)
-                            print("interval (left): mean = {:.4f}, pstdev = {:.4f} ({:.2f}%)".format(mu, pstd, pstd * 100 / mu))
+                            print(
+                                "interval (left): mean = {:.4f}, pstdev = {:.4f} ({:.2f}%)".format(
+                                    mu, pstd, pstd * 100 / mu
+                                )
+                            )
 
-                        print("interval: mean = {:.4f}, pstdev = {:.4f} ({:.2f}%)".format(mu_int, pstd_int, pstd_int * 100 / mu_int))
-                        print("velocity: mean = {:.4f}, pstdev = {:.4f} ({:.2f}%)".format(mu_vel, pstd_vel, pstd_vel * 100 / mu_vel))
-                        print("duration: mean = {:.4f}, pstdev = {:.4f} ({:.2f}%)".format(mu_dur, pstd_dur, pstd_dur * 100 / mu_dur))
+                        print(
+                            "interval: mean = {:.4f}, pstdev = {:.4f} ({:.2f}%)".format(
+                                mu_int, pstd_int, pstd_int * 100 / mu_int
+                            )
+                        )
+                        print(
+                            "velocity: mean = {:.4f}, pstdev = {:.4f} ({:.2f}%)".format(
+                                mu_vel, pstd_vel, pstd_vel * 100 / mu_vel
+                            )
+                        )
+                        print(
+                            "duration: mean = {:.4f}, pstdev = {:.4f} ({:.2f}%)".format(
+                                mu_dur, pstd_dur, pstd_dur * 100 / mu_dur
+                            )
+                        )
 
                     print("-" * shutil.get_terminal_size().columns)
 
@@ -189,7 +238,9 @@ class AseqThread(threading.Thread):
                     note = event[7][1]
                     velocity = event[7][2]
                     notes_on[note] = len(data)
-                    data.append((timestamp, note_to_str(note), velocity, 0, note_to_color(note)))
+                    data.append(
+                        (timestamp, note_to_str(note), velocity, 0, note_to_color(note))
+                    )
                     data_updated = True
                 elif event[0] == 7:
                     note = event[7][1]
@@ -206,7 +257,13 @@ class AseqThread(threading.Thread):
                 elif event[0] == 36:
                     for k, v in notes_on.items():
                         duration = timestamp - data[v][0]
-                        data[v] = (data[v][0], data[v][1], data[v][2], duration, data[v][4])
+                        data[v] = (
+                            data[v][0],
+                            data[v][1],
+                            data[v][2],
+                            duration,
+                            data[v][4],
+                        )
                         data_updated = True
 
                 if data_updated:
@@ -228,16 +285,17 @@ def update():
     p.setXRange(xmin, xmin + ((math.ceil(xdelta) + 5 - 1) // 5) * 5)
     bg.setOpts(x0=list(l[0]), height=list(l[2]), width=list(l[3]), brushes=list(l[4]))
 
+
 app = QtGui.QApplication([])
 
-pg.setConfigOption('background', 'w')
-pg.setConfigOption('foreground', 'k')
+pg.setConfigOption("background", "w")
+pg.setConfigOption("foreground", "k")
 
 w = QtGui.QMainWindow()
 cw = pg.GraphicsLayoutWidget()
 w.showMaximized()
 w.setCentralWidget(cw)
-w.setWindowTitle('aseq visualizer')
+w.setWindowTitle("aseq visualizer")
 
 bg = pg.BarGraphItem(x=[], height=[], width=[], pen=pg.mkPen(style=QtCore.Qt.NoPen))
 
@@ -253,8 +311,9 @@ t = QtCore.QTimer()
 t.timeout.connect(update)
 t.start(50)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
-    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, "PYQT_VERSION"):
         app.exec_()
         running = False
